00e2359d75f74250b68d0b45fc5baf45fe20753a
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-06 08:43:03 +0100

    SIMD for parsing rules

--- src/day5.rs
+++ src/day5.rs
@@ -2,6 +2,11 @@
 #![feature(portable_simd)]
 #![feature(avx512_target_feature)]
 
+use std::simd::cmp::{SimdPartialEq, SimdPartialOrd};
+use std::simd::num::SimdInt;
+use std::simd::ptr::SimdMutPtr;
+use std::simd::{simd_swizzle, u64x4, u8x32, u8x64, usizex4, Simd};
+
 pub fn run(input: &str) -> i64 {
     part2(input) as i64
 }
@@ -14,27 +19,137 @@ pub fn part2(input: &str) -> u32 {
     unsafe { inner_part2(input) }
 }
 
-#[target_feature(enable = "popcnt,avx2,ssse3,bmi1,bmi2,lzcnt")]
-#[cfg_attr(avx512_available, target_feature(enable = "avx512vl"))]
-unsafe fn inner_part1(input: &str) -> u32 {
-    let mut map = [0u128; 100];
+static NLMASK: [u8; 32] = {
+    let mut mask = [0; 32];
+    mask[0 * 6] = b'\n';
+    mask[1 * 6] = b'\n';
+    mask[2 * 6] = b'\n';
+    mask[3 * 6] = b'\n';
+    mask
+};
 
-    let mut iter = input.as_bytes().iter();
+static MUL_BLOCK: [u8; 32] = {
+    let mut block = [0; 32];
+    let mut i = 0;
+    while i < 4 {
+        block[6 * i] = 10;
+        block[6 * i + 1] = 1;
+        block[6 * i + 3] = 10;
+        block[6 * i + 4] = 1;
+        i += 1;
+    }
+    block
+};
+
+static SWIZZLE_BLOCK: [usize; 32] = {
+    let mut block = [31; 32];
+    let mut i = 0;
+    while i < 4 {
+        block[6 * i] = 6 * i + 1;
+        block[6 * i + 3] = 6 * i + 4;
+        i += 1;
+    }
+    block
+};
+
+static SWIZZLE_LEFT: [usize; 32] = {
+    let mut block = [31; 32];
+    let mut i = 0;
+    while i < 4 {
+        block[8 * i] = 6 * i;
+        i += 1;
+    }
+    block
+};
+
+static SWIZZLE_RIGHT: [usize; 32] = {
+    let mut block = [31; 32];
+    let mut i = 0;
+    while i < 4 {
+        block[8 * i] = 6 * i + 3;
+        i += 1;
+    }
+    block
+};
+
+#[inline(always)]
+unsafe fn read_rules(rules: &mut [u128; 100], iter: &mut std::slice::Iter<u8>) {
+    loop {
+        let block = u8x32::from_slice(iter.as_slice().get_unchecked(..32));
+        if block.simd_eq(u8x32::from_slice(&NLMASK)).any() {
+            break;
+        }
+
+        let digits = (block - u8x32::splat(b'0')) * u8x32::from_slice(&MUL_BLOCK);
+        let nums = digits + simd_swizzle!(digits, SWIZZLE_BLOCK);
+        let left = std::mem::transmute::<_, usizex4>(simd_swizzle!(nums, SWIZZLE_LEFT));
+        let right = std::mem::transmute::<_, u64x4>(simd_swizzle!(nums, SWIZZLE_RIGHT));
+        let right_big = right.simd_ge(u64x4::splat(64));
+        let idx = left * usizex4::splat(2) + (right_big.to_int().cast() & usizex4::splat(1));
+        let ptr = Simd::splat(rules.as_mut_ptr().cast::<u64>()).wrapping_add(idx);
+        let shift_amt = right & u64x4::splat(0b111111);
+        let to_store = u64x4::splat(1) << shift_amt;
+
+        for (&ptr, &to_store) in std::iter::zip(ptr.as_array(), to_store.as_array()) {
+            *ptr |= to_store;
+        }
+
+        *iter = iter.as_slice().get_unchecked(24..).iter();
+    }
 
     while *iter.as_slice().get_unchecked(0) != b'\n' {
         let c = iter.as_slice().get_unchecked(..5);
         let d1 = ((c[0] - b'0') as usize * 10) + ((c[1] - b'0') as usize);
         let d2 = ((c[3] - b'0') as usize * 10) + ((c[4] - b'0') as usize);
-        *map.get_unchecked_mut(d1) |= 1 << d2;
-        iter = iter.as_slice().get_unchecked(6..).iter();
+        *rules.get_unchecked_mut(d1) |= 1 << d2;
+        *iter = iter.as_slice().get_unchecked(6..).iter();
     }
+    *iter = iter.as_slice().get_unchecked(1..).iter();
+}
 
-    iter = iter.as_slice().get_unchecked(1..).iter();
+#[target_feature(enable = "popcnt,avx2,ssse3,bmi1,bmi2,lzcnt")]
+#[cfg_attr(avx512_available, target_feature(enable = "avx512vl"))]
+unsafe fn inner_part1(input: &str) -> u32 {
+    let mut iter = input.as_bytes().iter();
+
+    let mut rules = [0u128; 100];
+    read_rules(&mut rules, &mut iter);
 
     let mut tot = 0;
     let mut buf = [0; 24];
     let mut buf_len = 0;
     let mut mask = 0u128;
+
+    'outer: while iter.len() >= 72 {
+        buf_len = 0;
+        mask = 0;
+
+        let len = 8 + u8x64::from_slice(iter.as_slice().get_unchecked(8..8 + 64))
+            .simd_eq(u8x64::splat(b'\n'))
+            .first_set()
+            .unwrap_unchecked();
+
+        let mut c_iter = iter.as_slice().get_unchecked(..len + 1).iter();
+
+        iter = iter.as_slice().get_unchecked(len + 1..).iter();
+
+        while !c_iter.as_slice().is_empty() {
+            let c = c_iter.as_slice();
+
+            let n = (*c.get_unchecked(0) - b'0') * 10 + (*c.get_unchecked(1) - b'0');
+            *buf.get_unchecked_mut(buf_len) = n;
+            buf_len += 1;
+
+            if *rules.get_unchecked(n as usize) & mask != 0 {
+                continue 'outer;
+            }
+
+            mask |= 1 << n;
+            c_iter = c_iter.as_slice().get_unchecked(3..).iter();
+        }
+
+        tot += *buf.get_unchecked(buf_len / 2) as u32;
+    }
 
     'outer: while !iter.as_slice().is_empty() {
         let c = iter.as_slice();
@@ -43,7 +158,7 @@ unsafe fn inner_part1(input: &str) -> u32 {
         *buf.get_unchecked_mut(buf_len) = n;
         buf_len += 1;
 
-        if *map.get_unchecked(n as usize) & mask != 0 {
+        if *rules.get_unchecked(n as usize) & mask != 0 {
             while *iter.as_slice().get_unchecked(2) != b'\n' {
                 iter = iter.as_slice().get_unchecked(3..).iter();
             }
@@ -70,19 +185,10 @@ unsafe fn inner_part1(input: &str) -> u32 {
 #[target_feature(enable = "popcnt,avx2,ssse3,bmi1,bmi2,lzcnt")]
 #[cfg_attr(avx512_available, target_feature(enable = "avx512vl"))]
 unsafe fn inner_part2(input: &str) -> u32 {
-    let mut map = [0u128; 100];
-
     let mut iter = input.as_bytes().iter();
 
-    while *iter.as_slice().get_unchecked(0) != b'\n' {
-        let c = iter.as_slice().get_unchecked(..5);
-        let d1 = ((c[0] - b'0') as usize * 10) + ((c[1] - b'0') as usize);
-        let d2 = ((c[3] - b'0') as usize * 10) + ((c[4] - b'0') as usize);
-        *map.get_unchecked_mut(d1) |= 1 << d2;
-        iter = iter.as_slice().get_unchecked(6..).iter();
-    }
-
-    iter = iter.as_slice().get_unchecked(1..).iter();
+    let mut rules = [0u128; 100];
+    read_rules(&mut rules, &mut iter);
 
     let mut tot = 0;
     let mut buf = [0; 24];
@@ -94,13 +200,13 @@ unsafe fn inner_part2(input: &str) -> u32 {
         let n = (c[0] - b'0') * 10 + (c[1] - b'0');
         *buf.get_unchecked_mut(buf_len) = n;
         buf_len += 1;
-        valid &= *map.get_unchecked(n as usize) & mask == 0;
+        valid &= *rules.get_unchecked(n as usize) & mask == 0;
         mask |= 1 << n;
 
         if c[2] == b'\n' {
             if !valid {
                 for i in 0..buf_len {
-                    let succs = *map.get_unchecked(*buf.get_unchecked(i) as usize) & mask;
+                    let succs = *rules.get_unchecked(*buf.get_unchecked(i) as usize) & mask;
                     if succs.count_ones() == buf_len as u32 / 2 {
                         tot += *buf.get_unchecked(i) as u32;
                         break;
