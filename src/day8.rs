172293382702f9947ca1efce3cc9079c57ef5724
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-08 15:13:55 +0100

    d8p2: merge loops

--- src/day8.rs
+++ src/day8.rs
@@ -7,7 +7,7 @@ use std::mem::MaybeUninit;
 use std::simd::prelude::*;
 
 pub fn run(input: &str) -> i64 {
-    part1(input) as i64
+    part2(input) as i64
 }
 
 pub fn part1(input: &str) -> u64 {
@@ -82,14 +82,24 @@ unsafe fn inner_part1(input: &str) -> u64 {
 #[cfg_attr(avx512_available, target_feature(enable = "avx512vl"))]
 unsafe fn inner_part2(input: &str) -> u64 {
     let input = input.as_bytes();
-    let mut positions = [[(0, 0); 8]; 128];
+    let mut positions = [[MaybeUninit::<(u8, u8)>::uninit(); 8]; 128];
     let mut lengths = [0; 128];
 
-    for y in 0..49 {
+    let mut y = 0;
+
+    loop {
         let offset = 51 * y;
-        let block = u8x64::from_slice(input.get_unchecked(offset..offset + 64));
-        let mask = block.simd_ne(u8x64::splat(b'.'));
-        let mut mask = mask.to_bitmask() & ((1 << 50) - 1);
+        let mut mask;
+        if y < 49 {
+            let block = u8x64::from_slice(input.get_unchecked(offset..offset + 64));
+            mask = block.simd_ne(u8x64::splat(b'.')).to_bitmask() & ((1 << 50) - 1);
+        } else if y == 49 {
+            let block = u8x64::from_slice(input.get_unchecked(input.len() - 65..));
+            mask = block.simd_ne(u8x64::splat(b'.')).to_bitmask() >> 14;
+        } else {
+            break;
+        }
+
         while mask != 0 {
             let x = mask.trailing_zeros();
             mask &= !(1 << x);
@@ -98,59 +108,31 @@ unsafe fn inner_part2(input: &str) -> u64 {
             let len = lengths.get_unchecked_mut(b as usize);
             *positions
                 .get_unchecked_mut(b as usize)
-                .get_unchecked_mut(*len) = (x as u8, y as u8);
+                .get_unchecked_mut(*len) = MaybeUninit::new((x as u8, y as u8));
             *len += 1;
         }
-    }
-    {
-        let y = 49;
-        let offset = 51 * y;
-        let block = u8x64::from_slice(input.get_unchecked(input.len() - 65..));
-        let mask = block.simd_ne(u8x64::splat(b'.'));
-        let mut mask = mask.to_bitmask() >> 14;
-        while mask != 0 {
-            let x = mask.trailing_zeros();
-            mask &= !(1 << x);
 
-            let b = *input.get_unchecked(offset + x as usize);
-            let len = lengths.get_unchecked_mut(b as usize);
-            *positions
-                .get_unchecked_mut(b as usize)
-                .get_unchecked_mut(*len) = (x as u8, y as u8);
-            *len += 1;
-        }
+        y += 1;
     }
-
-    // for (i, (&len, positions)) in std::iter::zip(&lengths, &positions).enumerate() {
-    //     if len != 0 {
-    //         println!("{}: {:?}", i as u8 as char, &positions[..len]);
-    //     }
-    // }
 
     let mut marked = [false; 64 * 50];
     let mut count = 0;
     for (&len, positions) in std::iter::zip(&lengths, &positions) {
         for i in 0..len {
-            let (xi, yi) = *positions.get_unchecked(i);
+            let (xi, yi) = positions.get_unchecked(i).assume_init();
             for j in i + 1..len {
-                let (xj, yj) = *positions.get_unchecked(j);
+                let (xj, yj) = positions.get_unchecked(j).assume_init();
                 let (dx, dy) = (xj.wrapping_sub(xi), yj.wrapping_sub(yi));
 
                 let (mut xa, mut ya) = (xi, yi);
-                loop {
-                    if xa >= 50 || ya >= 50 {
-                        break;
-                    }
+                while xa < 50 && ya < 50 {
                     count += !marked.get_unchecked((xa as usize * 64) + ya as usize) as u64;
                     *marked.get_unchecked_mut((xa as usize * 64) + ya as usize) = true;
                     (xa, ya) = (xa.wrapping_sub(dx), ya.wrapping_sub(dy));
                 }
 
                 let (mut xa, mut ya) = (xj, yj);
-                loop {
-                    if xa >= 50 || ya >= 50 {
-                        break;
-                    }
+                while xa < 50 && ya < 50 {
                     count += !marked.get_unchecked((xa as usize * 64) + ya as usize) as u64;
                     *marked.get_unchecked_mut((xa as usize * 64) + ya as usize) = true;
                     (xa, ya) = (xa.wrapping_add(dx), ya.wrapping_add(dy));
