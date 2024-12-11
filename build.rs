b61fdb3a79b9ebb21c81da303bce17fdffbb9e13
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-11 09:04:02 +0100

    d11: better lut

--- build.rs
+++ build.rs
@@ -1,6 +1,58 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
+
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(avx512_available)");
     if is_x86_feature_detected!("avx512vl") {
         println!("cargo::rustc-cfg=avx512_available");
     }
+
+    make_d11_lut(
+        25,
+        &Path::new(&std::env::var("OUT_DIR").unwrap()).join("d11p1.lut"),
+    );
+    make_d11_lut(
+        75,
+        &Path::new(&std::env::var("OUT_DIR").unwrap()).join("d11p2.lut"),
+    );
+}
+
+fn make_d11_lut(iters: usize, path: &Path) {
+    if path.exists() {
+        return;
+    }
+
+    let mut levels = vec![HashMap::new(); iters];
+
+    fn solve_rec(i: usize, j: usize, levels: &mut [HashMap<usize, usize>]) -> usize {
+        if i == 0 {
+            return 1;
+        }
+
+        if let Some(&res) = levels[i - 1].get(&j) {
+            return res;
+        }
+
+        let res = if j == 0 {
+            solve_rec(i - 1, 1, levels)
+        } else if j.ilog10() % 2 == 1 {
+            let pow10 = 10usize.pow((j.ilog10() + 1) / 2);
+            solve_rec(i - 1, j / pow10, levels) + solve_rec(i - 1, j % pow10, levels)
+        } else {
+            solve_rec(i - 1, j * 2024, levels)
+        };
+
+        levels[i - 1].insert(j, res);
+
+        res
+    }
+
+    let mut out = BufWriter::new(File::create(path).unwrap());
+    for j in 0..10_000_000 {
+        let n = solve_rec(iters, j, &mut levels);
+        out.write(&n.to_ne_bytes()).unwrap();
+    }
+    out.flush().unwrap();
 }
--- src/day11.rs
+++ src/day11.rs
@@ -18,55 +18,7 @@ pub fn part2(input: &str) -> u64 {
     unsafe { inner_part2(input) }
 }
 
-#[allow(long_running_const_eval)]
-const fn solve_rec(i: usize, j: usize, lut: &[[u64; 1000]; 76]) -> u64 {
-    if i == 0 {
-        return 1;
-    } else if j < 1000 {
-        return lut[i][j];
-    } else if j == 0 {
-        return lut[i - 1][1];
-    } else if j.ilog10() % 2 == 1 {
-        let pow10 = 10usize.pow((j.ilog10() + 1) / 2);
-        return solve_rec(i - 1, j / pow10, lut) + solve_rec(i - 1, j % pow10, lut);
-    } else {
-        return solve_rec(i - 1, j * 2024, lut);
-    }
-}
-
-#[allow(long_running_const_eval)]
-static LUT: [[u64; 1000]; 76] = {
-    let mut lut = [[0; 1000]; 76];
-
-    let mut j = 0;
-    while j < 1000 {
-        lut[0][j] = 1;
-        j += 1;
-    }
-
-    let mut i = 1;
-    while i < 76 {
-        let mut j = 0;
-        while j < 1000 {
-            if j == 0 {
-                lut[i][j] = lut[i - 1][1];
-            } else if j.ilog10() % 2 == 1 {
-                let pow10 = 10usize.pow((j.ilog10() + 1) / 2);
-                lut[i][j] = lut[i - 1][j / pow10] + lut[i - 1][j % pow10];
-            } else {
-                lut[i][j] = solve_rec(i - 1, j * 2024, &lut);
-            }
-
-            j += 1;
-        }
-
-        i += 1;
-    }
-
-    lut
-};
-
-unsafe fn solve(input: &[u8], iters: usize) -> u64 {
+unsafe fn solve(input: &[u8], lut: &[u8]) -> u64 {
     let mut iter = input.iter();
     let mut tot = 0;
 
@@ -82,7 +34,7 @@ unsafe fn solve(input: &[u8], iters: usize) -> u64 {
             n = 10 * n + d as usize;
         }
 
-        tot += solve_rec(iters, n, &LUT);
+        tot += u64::from_ne_bytes(lut.get_unchecked(8 * n..8 * n + 8).try_into().unwrap());
     }
 
     tot
@@ -91,11 +43,17 @@ unsafe fn solve(input: &[u8], iters: usize) -> u64 {
 #[target_feature(enable = "popcnt,avx2,ssse3,bmi1,bmi2,lzcnt")]
 #[cfg_attr(avx512_available, target_feature(enable = "avx512vl"))]
 unsafe fn inner_part1(input: &str) -> u64 {
-    solve(input.as_bytes(), 25)
+    solve(
+        input.as_bytes(),
+        include_bytes!(concat!(env!("OUT_DIR"), "/d11p1.lut")),
+    )
 }
 
 #[target_feature(enable = "popcnt,avx2,ssse3,bmi1,bmi2,lzcnt")]
 #[cfg_attr(avx512_available, target_feature(enable = "avx512vl"))]
 unsafe fn inner_part2(input: &str) -> u64 {
-    solve(input.as_bytes(), 75)
+    solve(
+        input.as_bytes(),
+        include_bytes!(concat!(env!("OUT_DIR"), "/d11p2.lut")),
+    )
 }
