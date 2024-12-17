546d1ad576d623c0cc2508dd6b66f3088375179c
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-17 15:02:57 +0100

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
+use std::sync::OnceLock;
+
 fn bench<D: std::fmt::Display>(mut f: impl FnMut() -> D) {
+    let sol = f();
     let n = if cfg!(debug_assertions) { 1 } else { 100 };
     let now = std::time::Instant::now();
-    for _ in 0..n - 1 {
+    for _ in 0..n {
         f();
     }
-    let sol = f();
     println!("Took: {:?}", now.elapsed() / n);
     println!("Solution: {sol}");
 }
 
+static INPUT: OnceLock<String> = OnceLock::new();
+
 macro_rules! run {
     ($day:ident, $part:ident) => {
-        advent_of_codspeed_2024::$day::$part(include_str!(concat!(
-            "../input/2024/",
-            stringify!($day),
-            ".txt"
-        )))
+        advent_of_codspeed_2024::$day::$part(INPUT.get_or_init(|| {
+            std::fs::read_to_string(concat!("input/2024/", stringify!($day), ".txt")).unwrap()
+        }))
     };
 }
 
