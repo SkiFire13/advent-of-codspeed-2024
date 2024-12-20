20bd702eb2f6dbde9a0194e2985313fd8ab3a7d2
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-20 09:34:54 +0100

    Main: avoid running parts twice in debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use std::sync::OnceLock;
 
 fn bench<D: std::fmt::Display>(mut f: impl FnMut() -> D) {
-    let sol = f();
     let n = if cfg!(debug_assertions) { 1 } else { 10000 };
     let now = std::time::Instant::now();
-    for _ in 0..n {
+    for _ in 1..n {
         f();
     }
+    let sol = f();
     println!("Took: {:?}", now.elapsed() / n);
     println!("Solution: {sol}");
 }
 
-static INPUT: OnceLock<&'static str> = OnceLock::new();
-
 macro_rules! run {
-    ($day:ident, $part:ident) => {
-        advent_of_codspeed_2024::$day::$part(INPUT.get_or_init(|| {
+    ($day:ident) => {{
+        static INPUT: OnceLock<&'static str> = OnceLock::new();
+        let input = INPUT.get_or_init(|| {
             #[cfg(not(miri))]
             {
                 std::fs::read_to_string(concat!("input/2024/", stringify!($day), ".txt"))
@@ -27,11 +26,13 @@ macro_rules! run {
             {
                 include_str!(concat!("../input/2024/", stringify!($day), ".txt"))
             }
-        }))
-    };
+        });
+
+        bench(|| advent_of_codspeed_2024::$day::part1(input));
+        bench(|| advent_of_codspeed_2024::$day::part2(input));
+    }};
 }
 
 fn main() {
-    bench(|| run!(day19, part1));
-    bench(|| run!(day19, part2));
+    run!(day19)
 }
