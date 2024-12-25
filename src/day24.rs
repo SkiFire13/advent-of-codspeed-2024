e24d6b04a7ab62770a685f86db53a71383196baf
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-25 17:51:08 +0100

    d24: various improvements

--- src/day24.rs
+++ src/day24.rs
@@ -6,6 +6,10 @@
 #![feature(array_ptr_get)]
 #![feature(core_intrinsics)]
 #![feature(int_roundings)]
+#![feature(fn_align)]
+
+use std::arch::x86_64::*;
+use std::simd::prelude::*;
 
 pub fn run(input: &str) -> i64 {
     part1(input) as i64
@@ -16,118 +20,114 @@ pub fn run(input: &str) -> i64 {
 // }
 
 #[inline(always)]
+#[repr(align(64))]
 pub fn part1(input: &str) -> u64 {
     unsafe { inner_part1(input) }
 }
 
 #[inline(always)]
+#[repr(align(64))]
 pub fn part2(input: &str) -> &'static str {
     unsafe { inner_part2(input) }
 }
 
 #[target_feature(enable = "popcnt,avx2,ssse3,bmi1,bmi2,lzcnt")]
 #[cfg_attr(avx512_available, target_feature(enable = "avx512vl"))]
+#[repr(align(64))]
 unsafe fn inner_part1(input: &str) -> u64 {
     let input = input.as_bytes();
 
-    let mut xids = [u8::MAX; 45];
-    let mut yids = [u8::MAX; 45];
+    const BASE: usize = (23usize * 26 * 26).next_multiple_of(45) + 2 * 45;
+
+    const {
+        assert!(BASE / 45 == (b' ' as usize - 28) * 87);
+    }
+
+    static mut OPS: [[u16; 3]; BASE + 45 + 45 + 46] = [[u16::MAX; 3]; BASE + 45 + 45 + 46];
+    let ops = &mut OPS;
+
+    static mut VALUES: [u8; BASE + 45 + 45 + 46] = [u8::MAX; BASE + 45 + 45 + 46];
+    let values = &mut VALUES;
 
     let mut ptr = input.as_ptr();
     for i in 0..45 {
-        xids[i] = (*ptr.add(5) - b'0') + 46;
+        values[BASE + i] = *ptr.add(5) - b'0';
         ptr = ptr.add(7);
     }
     for i in 0..45 {
-        yids[i] = (*ptr.add(5) - b'0') + 46;
+        values[BASE + 45 + i] = *ptr.add(5) - b'0';
         ptr = ptr.add(7);
     }
     ptr = ptr.add(1);
 
-    let mut node_to_id = [u8::MAX; 23 * 26 * 26];
-    let mut next_id = 48;
-
-    macro_rules! get_id {
-        ($a:ident, $b:ident, $c:ident) => {{
-            let node =
-                26 * 26 * ($a - b'a' as usize) + 26 * ($b - b'a' as usize) + ($c - b'a' as usize);
-            let mut id = *node_to_id.get_unchecked(node);
-            if id == u8::MAX {
-                id = next_id;
-                *node_to_id.get_unchecked_mut(node) = id;
-                next_id += 1;
-            }
-            id
-        }};
-    }
-
-    static mut OPS: [[u8; 3]; 224] = [[u8::MAX; 3]; 224];
-    let ops = &mut OPS;
-
     let end = input.as_ptr().add(input.len());
     loop {
-        let a = *ptr as usize;
-        let b = *ptr.add(1) as usize;
-        let c = *ptr.add(2) as usize;
-        ptr = ptr.add(4);
+        let o = *ptr.add(4) as usize;
 
-        let (n, op, m) = if a == b'x' as usize {
-            let idx = 10 * (b - b'0' as usize) + (c - b'0' as usize);
-            let n = *xids.get_unchecked(idx);
-            let m = *yids.get_unchecked(idx);
-            let op = (*ptr == b'X') as u8;
-            ptr = ptr.add(11);
-            (n, op, m)
-        } else if a == b'y' as usize {
-            let idx = 10 * (b - b'0' as usize) + (c - b'0' as usize);
-            let n = *yids.get_unchecked(idx);
-            let m = *xids.get_unchecked(idx);
-            let op = (*ptr == b'X') as u8;
-            ptr = ptr.add(11);
-            (n, op, m)
-        } else {
-            let n = get_id!(a, b, c);
+        let mut b = ptr.cast::<u8x16>().read_unaligned();
+        b[12] = *ptr.add(16);
+        b[13] = *ptr.add(17);
 
-            let op = *ptr;
-            let op = if op == b'O' {
-                ptr = ptr.add(3);
-                2
-            } else {
-                ptr = ptr.add(4);
-                (op == b'X') as u8
-            };
+        #[rustfmt::skip]
+        static LUT_SWIZZLE: [u8x16; 3] = [
+            u8x16::from_array([3, 0, 1, 2,  3, 8, 9, 10,  3, 15, 12, 13,  0, 0, 0, 0]),
+            u8x16::from_array([0; 16]),
+            u8x16::from_array([3, 0, 1, 2,  3, 7, 8, 9,   3, 14, 15, 12,  0, 0, 0, 0]),
+        ];
+        let swizzle = *LUT_SWIZZLE.get_unchecked(o & 0b10);
+        let b = u8x16::from(_mm_shuffle_epi8(b.into(), swizzle.into()));
 
-            let a = *ptr as usize;
-            let b = *ptr.add(1) as usize;
-            let c = *ptr.add(2) as usize;
-            ptr = ptr.add(7);
+        static LOFF_LUT: [usize; 3] = [1, 1, 0];
+        let loff = *LOFF_LUT.get_unchecked(o & 0b10);
+        let f = *ptr.add(0);
+        let l = *ptr.add(loff + 14);
+        let idx = 2 * (f >= b'x') as usize + (l == b'z') as usize;
 
-            let m = get_id!(a, b, c);
+        #[rustfmt::skip]
+        static LUT_SUB: [u8x16; 4] = [
+            u8x16::from_array([ 0, b'a', b'a', b'a',   0, b'a', b'a', b'a',   0, b'a', b'a', b'a',  0, 0, 0, 0]),
+            u8x16::from_array([ 0, b'a', b'a', b'a',   0, b'a', b'a', b'a',  28, b'x', b'0', b'0',  0, 0, 0, 0]),
+            u8x16::from_array([28, b'x', b'0', b'0',  28, b'x', b'0', b'0',   0, b'a', b'a', b'a',  0, 0, 0, 0]),
+            u8x16::from_array([28, b'x', b'0', b'0',  28, b'x', b'0', b'0',  28, b'x', b'0', b'0',  0, 0, 0, 0]),
+        ];
+        let sub = *LUT_SUB.get_unchecked(idx);
+        let b = b - sub;
 
-            (n, op, m)
-        };
+        #[rustfmt::skip]
+        static LUT_MUL: [u8x16; 4] = [
+            u8x16::from_array([0, 26, 26, 1,  0, 26, 26, 1,  0, 26, 26, 1,  0, 0, 0, 0]),
+            u8x16::from_array([0, 26, 26, 1,  0, 26, 26, 1,  87, 1, 10, 1,  0, 0, 0, 0]),
+            u8x16::from_array([87, 1, 10, 1,  87, 1, 10, 1,  0, 26, 26, 1,  0, 0, 0, 0]),
+            u8x16::from_array([87, 1, 10, 1,  87, 1, 10, 1,  87, 1, 10, 1,  0, 0, 0, 0]),
+        ];
+        let mul = *LUT_MUL.get_unchecked(idx);
+        let b = u16x8::from(_mm_maddubs_epi16(b.into(), mul.into()));
 
-        let a = *ptr as usize;
-        let b = *ptr.add(1) as usize;
-        let c = *ptr.add(2) as usize;
-        ptr = ptr.add(4);
+        static LUT_MUL2: [u16x8; 4] = [
+            u16x8::from_array([26, 1, 26, 1, 26, 1, 0, 0]),
+            u16x8::from_array([26, 1, 26, 1, 45, 1, 0, 0]),
+            u16x8::from_array([45, 1, 45, 1, 26, 1, 0, 0]),
+            u16x8::from_array([45, 1, 45, 1, 45, 1, 0, 0]),
+        ];
+        let mul2 = *LUT_MUL2.get_unchecked(idx);
+        let b = u32x4::from(_mm_madd_epi16(b.into(), mul2.into()));
 
-        let out = if a == b'z' as usize {
-            (10 * (b - b'0' as usize) + (c - b'0' as usize)) as u8
-        } else {
-            get_id!(a, b, c)
-        };
+        debug_assert!(b[0] < BASE as u32 + 45 + 45 + 46, "{}", b[0]);
+        debug_assert!(b[1] < BASE as u32 + 45 + 45 + 46, "{}", b[1]);
+        debug_assert!(b[2] < BASE as u32 + 45 + 45 + 46, "{}", b[2]);
+        let (l, r, out) = (b[0] as u16, b[1] as u16, b[2] as u16);
+        static LUT_OP: [u16; 4] = [1, 0, 0, 2];
+        let op = *LUT_OP.get_unchecked(o & 0b11);
 
-        *ops.get_unchecked_mut(out as usize) = [n, op, m];
+        *ops.get_unchecked_mut(out as usize) = [l, op, r];
+        *values.get_unchecked_mut(out as usize) = u8::MAX;
+
+        ptr = ptr.add(18 + loff);
 
         if ptr == end {
             break;
         }
     }
-
-    let mut values = [u8::MAX; 224];
-    values[46] = 0;
-    values[47] = 1;
 
     let mut out = 0;
 
@@ -138,6 +138,8 @@ unsafe fn inner_part1(input: &str) -> u64 {
                 let [l, op, r] = *ops.get_unchecked(n);
                 let l = calc_rec!([fuel: $($fuel)*] l);
                 let r = calc_rec!([fuel: $($fuel)*] r);
+                debug_assert!(l == 0 || l == 1, "{l}");
+                debug_assert!(r == 0 || r == 1, "{l}");
                 match op {
                     0 => l & r,
                     1 => l ^ r,
@@ -159,7 +161,7 @@ unsafe fn inner_part1(input: &str) -> u64 {
             }};
         }
 
-        out |= (calc_rec!(force [fuel: f f f f] z) as u64) << z;
+        out |= (calc_rec!(force [fuel: f f f f] BASE + 45 + 45 + z) as u64) << z;
     }
 
     out
@@ -173,8 +175,10 @@ static mut PART2_OUT: [u8; 8 * 3 + 7] = [b','; 8 * 3 + 7];
 unsafe fn inner_part2(input: &str) -> &'static str {
     let input = input.as_bytes();
 
-    let mut node_to_id = [u8::MAX; 23 * 26 * 26];
-    let mut id_to_node = [u16::MAX; 222];
+    static mut NODE_TO_ID: [u8; 23 * 26 * 26] = [u8::MAX; 23 * 26 * 26];
+    let node_to_id = &mut NODE_TO_ID;
+    static mut ID_TO_NODE: [u16; 222] = [u16::MAX; 222];
+    let id_to_node = &mut ID_TO_NODE;
     let mut next_id = 46;
 
     static mut XYOPS: [[u8; 2]; 45] = [[u8::MAX; 2]; 45];
@@ -216,7 +220,7 @@ unsafe fn inner_part2(input: &str) -> &'static str {
         let c = *ptr.add(2) as usize;
         ptr = ptr.add(4);
 
-        if a == b'x' as usize || a == b'y' as usize {
+        if a >= b'x' as usize {
             let n = 10 * (b - b'0' as usize) + (c - b'0' as usize);
             let off = (*ptr == b'X') as usize;
 
@@ -341,6 +345,11 @@ unsafe fn inner_part2(input: &str) -> &'static str {
     }
 
     debug_assert_eq!(out_len, 8);
+
+    for i in 46..next_id as usize {
+        let n = *id_to_node.get_unchecked(i) as usize;
+        *node_to_id.get_unchecked_mut(n) = u8::MAX;
+    }
 
     let mut out_chr = [[u8::MAX; 3]; 8];
     for i in 0..8 {
