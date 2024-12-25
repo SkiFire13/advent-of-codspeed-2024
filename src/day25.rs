bef9d732eb7db4f445b367bf121a85f30e209ab7
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-25 18:18:08 +0100

    d25: simplify a bit

--- src/day25.rs
+++ src/day25.rs
@@ -32,29 +32,23 @@ unsafe fn inner_part1(input: &str) -> u64 {
     let input = input.as_bytes();
 
     #[repr(align(64))]
-    struct Aligned([u32; 256]);
-
-    static mut PREVS0: Aligned = Aligned([0; 256]);
-    static mut PREVS1: Aligned = Aligned([0; 256]);
-
-    let prevs0 = &mut PREVS0.0;
-    let mut prevs0_len = 0;
-    let prevs1 = &mut PREVS1.0;
-    let mut prevs1_len = 0;
+    struct Aligned([u32; 512]);
+    static mut PREVS: Aligned = Aligned([0; 512]);
+    let (list0, list1) = PREVS.0.split_at_mut(256);
+    let (mut len0, mut len1) = (0, 0);
 
     let mut count = i32x8::splat(0);
 
     let mut ptr = input.as_ptr();
     let end = ptr.add(input.len());
-
     loop {
         let b = ptr.add(4).cast::<u8x32>().read_unaligned();
         let m = b.simd_eq(u8x32::splat(b'#')).to_bitmask() as u32;
 
         let (prevsi, prevsi_len, prevsj, prevsj_len) = if *ptr.add(4) == b'#' {
-            (&*prevs0, prevs0_len, &mut *prevs1, &mut prevs1_len)
+            (&*list0, len0, &mut *list1, &mut len1)
         } else {
-            (&*prevs1, prevs1_len, &mut *prevs0, &mut prevs0_len)
+            (&*list1, len1, &mut *list0, &mut len0)
         };
 
         *prevsj.get_unchecked_mut(*prevsj_len) = m;
@@ -80,17 +74,19 @@ unsafe fn inner_part1(input: &str) -> u64 {
             }
 
             #[rustfmt::skip]
-            static MASKS: [u32x8; 9] = [
-                u32x8::from_array([u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,]),
-                u32x8::from_array([0,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,]),
-                u32x8::from_array([0,0,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,]),
-                u32x8::from_array([0,0,0,u32::MAX,u32::MAX,u32::MAX,u32::MAX,u32::MAX,]),
-                u32x8::from_array([0,0,0,0,u32::MAX,u32::MAX,u32::MAX,u32::MAX,]),
-                u32x8::from_array([0,0,0,0,0,u32::MAX,u32::MAX,u32::MAX,]),
-                u32x8::from_array([0,0,0,0,0,0,u32::MAX,u32::MAX,]),
-                u32x8::from_array([0,0,0,0,0,0,0,u32::MAX,]),
-                u32x8::from_array([0,0,0,0,0,0,0,0]),
-            ];
+            static MASKS: [u32x8; 9] = {
+                let mut masks = [[u32::MAX; 8]; 9];
+                let mut i = 0;
+                while i < 9 {
+                    let mut j = 0;
+                    while j < i {
+                        masks[i][j] = 0;
+                        j += 1;
+                    }
+                    i += 1;
+                }
+                unsafe { std::mem::transmute(masks) }
+            };
             let rem = end.cast::<u32>().offset_from(ptr.cast::<u32>()) as usize;
             let b = *ptr | *MASKS.get_unchecked(rem);
             count -= (b & m).simd_eq(z).to_int();
