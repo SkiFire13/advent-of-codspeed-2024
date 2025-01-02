5dd01d1952174020cc395c99c066aea24f0e118d
Giacomo Stevanato <giaco.stevanato@gmail.com>
2025-01-02 11:52:57 +0100

    d9p2: keep array of minimums

--- src/day9.rs
+++ src/day9.rs
@@ -1,10 +1,16 @@
 #![allow(unused_attributes)]
+#![allow(static_mut_refs)]
 #![feature(portable_simd)]
 #![feature(avx512_target_feature)]
 #![feature(slice_ptr_get)]
 #![feature(array_ptr_get)]
+#![feature(core_intrinsics)]
+#![feature(int_roundings)]
+#![feature(fn_align)]
 
+use std::arch::x86_64::*;
 use std::mem::MaybeUninit;
+use std::simd::prelude::*;
 
 pub fn run(input: &str) -> i64 {
     part2(input) as i64
@@ -82,48 +88,6 @@ unsafe fn inner_part2(input: &str) -> u64 {
         *queues.get_mut(i).get_mut(0).as_mut_ptr() = u16::MAX;
     }
 
-    macro_rules! queue_push {
-        ($i:expr, $elem:expr) => {{
-            let i = $i as usize;
-            let elem = $elem;
-            let len = queues_len.get_unchecked_mut(i);
-            let queue = queues.get_mut(i);
-            let mut pos = *len;
-            while *queue.get(pos - 1).as_ptr() < elem {
-                pos -= 1;
-            }
-            let ptr = queue.as_mut_ptr().cast::<u16>();
-            std::ptr::copy(ptr.add(pos), ptr.add(pos + 1), *len - pos);
-            *queue.get_mut(pos).as_mut_ptr() = elem;
-            *len += 1;
-        }};
-    }
-
-    macro_rules! bheap_push {
-        ($elem:expr) => {{
-            let len = queues_len.get_unchecked_mut(0);
-            let heap = queues.get_mut(0);
-            *heap.get_mut(*len).as_mut_ptr() = $elem;
-            *len += 1;
-            bheap::push(&mut *heap.as_mut_ptr().as_mut_slice().get_unchecked_mut(..*len));
-        }};
-    }
-
-    macro_rules! queue_pop {
-        ($i:expr) => {{
-            *queues_len.get_unchecked_mut($i as usize) -= 1;
-        }};
-    }
-
-    macro_rules! bheap_pop {
-        () => {{
-            let len = queues_len.get_unchecked_mut(0);
-            let heap = queues.get_mut(0);
-            bheap::pop(&mut *heap.as_mut_ptr().as_mut_slice().get_unchecked_mut(..*len));
-            *len -= 1;
-        }};
-    }
-
     let mut i = 19999;
     let mut j = 9999;
     loop {
@@ -169,6 +133,109 @@ unsafe fn inner_part2(input: &str) -> u64 {
     }
 
     queues_len[0] = 1;
+    let mut mins = [u16::MAX; 16];
+
+    mins[0] = *queues.get(0).get(0).as_ptr();
+    for i in 1..10 {
+        mins[i] = *queues.get(i).get(queues_len[i] - 1).as_ptr();
+    }
+
+    macro_rules! queue_push {
+        ($i:expr, $elem:expr) => {{
+            let i = $i as usize;
+            let elem = $elem;
+            let len = queues_len.get_unchecked_mut(i);
+            let queue = queues.get_mut(i);
+            let mut pos = *len;
+            while *queue.get(pos - 1).as_ptr() < elem {
+                pos -= 1;
+            }
+            let ptr = queue.as_mut_ptr().cast::<u16>();
+            std::ptr::copy(ptr.add(pos), ptr.add(pos + 1), *len - pos);
+            *queue.get_mut(pos).as_mut_ptr() = elem;
+            *len += 1;
+
+            let min = mins.get_unchecked_mut(i);
+            *min = (*min).min(elem);
+        }};
+    }
+
+    macro_rules! bheap_push {
+        ($elem:expr) => {{
+            let elem = $elem;
+            let len = queues_len.get_unchecked_mut(0);
+            let heap = queues.get_mut(0);
+            *heap.get_mut(*len).as_mut_ptr() = elem;
+            *len += 1;
+            bheap::push(&mut *heap.as_mut_ptr().as_mut_slice().get_unchecked_mut(..*len));
+
+            let min = mins.get_unchecked_mut(0);
+            *min = (*min).min(elem);
+        }};
+    }
+
+    macro_rules! queue_pop {
+        ($i:expr) => {{
+            let i = $i as usize;
+            let len = queues_len.get_unchecked_mut(i);
+            *len -= 1;
+
+            let min = mins.get_unchecked_mut(i);
+            *min = *queues.get(i).get(*len - 1).as_ptr();
+        }};
+    }
+
+    macro_rules! bheap_pop {
+        () => {{
+            let len = queues_len.get_unchecked_mut(0);
+            let heap = queues.get_mut(0);
+            bheap::pop(&mut *heap.as_mut_ptr().as_mut_slice().get_unchecked_mut(..*len));
+            *len -= 1;
+
+            *mins.get_unchecked_mut(0) = *heap.get(0).as_ptr();
+        }};
+    }
+
+    const MASKS: [u16x16; 10] = {
+        let mut masks = [[0; 16]; 10];
+
+        let mut i = 2;
+        while i < 10 {
+            let mut j = 0;
+            while j < i {
+                masks[i][j] = u16::MAX;
+                j += 1;
+            }
+
+            i += 1;
+        }
+
+        unsafe { std::mem::transmute(masks) }
+    };
+
+    macro_rules! minpos {
+        ($b:expr) => {{
+            let b = $b as usize;
+
+            let mins = u16x16::from_array(mins) | *MASKS.get_unchecked(b);
+
+            let res = u16x8::from(_mm_minpos_epu16(u16x8::from_slice(mins.as_array()).into()));
+            let mut min = res[0];
+            let mut pos = res[1];
+
+            if mins[8] < min {
+                min = mins[8];
+                pos = 8;
+            }
+
+            if mins[9] < min {
+                min = mins[9];
+                pos = 9;
+            }
+
+            (min, pos as usize)
+        }};
+    }
 
     let mut tot = 0;
 
@@ -178,51 +245,13 @@ unsafe fn inner_part2(input: &str) -> u64 {
     let mut posi = pos as usize + posj_base as usize;
     let total_pos = pos as usize;
     loop {
-        let b = *input.get_unchecked(i) - b'0';
+        let b = *input.get_unchecked(i) as usize - b'0' as usize;
 
-        posi -= b as usize;
+        posi -= b;
 
-        let mut min_j = u16::MAX;
-        let mut min_h = 0;
-        for h in (1..10).rev() {
-            if h >= b {
-                let j = *queues
-                    .get(h as usize)
-                    .get(queues_len[h as usize] - 1)
-                    .as_ptr();
-                if j < min_j {
-                    min_j = j;
-                    min_h = h;
-                }
-            }
-        }
+        let (min_j, min_h) = minpos!(b);
 
-        let j = *queues.get(0).get(0).as_ptr();
-        if b == 1 && j < min_j && (j as usize) < i / 2 {
-            min_j = j;
-
-            bheap_pop!();
-
-            let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
-            let len = b as usize;
-            tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
-            *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
-        } else if (min_j as usize) < i / 2 {
-            queue_pop!(min_h);
-
-            if min_h != b {
-                if min_h - b == 1 {
-                    bheap_push!(min_j);
-                } else {
-                    queue_push!(min_h - b, min_j);
-                }
-            }
-
-            let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
-            let len = b as usize;
-            tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
-            *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
-        } else {
+        if min_j as usize >= i / 2 {
             let len = b as usize;
             tot += (i / 2) * (len * (2 * posi + len - 1) / 2);
 
@@ -231,6 +260,26 @@ unsafe fn inner_part2(input: &str) -> u64 {
             i -= 2;
             posi -= (*input.get_unchecked(i + 1) - b'0') as usize;
             break;
+        } else if min_h == 0 {
+            bheap_pop!();
+
+            let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
+            let len = b as usize;
+            tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
+            *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
+        } else {
+            queue_pop!(min_h);
+
+            if min_h - b == 1 {
+                bheap_push!(min_j);
+            } else if min_h != b {
+                queue_push!(min_h - b, min_j);
+            }
+
+            let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
+            let len = b as usize;
+            tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
+            *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
         }
 
         i -= 2;
@@ -238,52 +287,14 @@ unsafe fn inner_part2(input: &str) -> u64 {
     }
 
     loop {
-        let b = *input.get_unchecked(i) - b'0';
+        let b = *input.get_unchecked(i) as usize - b'0' as usize;
 
         posi -= b as usize;
 
         if b < max_b {
-            let mut min_j = u16::MAX;
-            let mut min_h = 0;
-            for h in (1..10).rev() {
-                if h >= b {
-                    let j = *queues
-                        .get(h as usize)
-                        .get(queues_len[h as usize] - 1)
-                        .as_ptr();
-                    if j < min_j {
-                        min_j = j;
-                        min_h = h;
-                    }
-                }
-            }
+            let (min_j, min_h) = minpos!(b);
 
-            let j = *queues.get(0).get(0).as_ptr();
-            if b == 1 && j < min_j && (j as usize) < i / 2 {
-                min_j = j;
-
-                bheap_pop!();
-
-                let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
-                let len = b as usize;
-                tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
-                *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
-            } else if (min_j as usize) < i / 2 {
-                queue_pop!(min_h);
-
-                if min_h != b {
-                    if min_h - b == 1 {
-                        bheap_push!(min_j);
-                    } else {
-                        queue_push!(min_h - b, min_j);
-                    }
-                }
-
-                let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
-                let len = b as usize;
-                tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
-                *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
-            } else {
+            if min_j as usize >= i / 2 {
                 let len = b as usize;
                 tot += (i / 2) * (len * (2 * posi + len - 1) / 2);
                 max_b = b;
@@ -299,6 +310,26 @@ unsafe fn inner_part2(input: &str) -> u64 {
                     }
                     break;
                 }
+            } else if min_h == 0 {
+                bheap_pop!();
+
+                let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
+                let len = b as usize;
+                tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
+                *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
+            } else {
+                queue_pop!(min_h);
+
+                if min_h - b == 1 {
+                    bheap_push!(min_j);
+                } else if min_h != b {
+                    queue_push!(min_h - b, min_j);
+                }
+
+                let pos = total_pos - *poss.get(min_j as usize).as_ptr() as usize;
+                let len = b as usize;
+                tot += (i / 2) * (len * (2 * pos + len - 1) / 2);
+                *poss.get_mut(min_j as usize).as_mut_ptr() -= b as u16;
             }
         } else {
             let len = b as usize;
